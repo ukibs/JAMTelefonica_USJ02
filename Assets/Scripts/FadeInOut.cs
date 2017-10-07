13f26af33f3f7ea450d19a35b757c19cfb4d79b3
ukibs <martincativiela@gmail.com>
2017-10-07 21:32:21 +0200

    Montado templo del sur y orbe para finalizar (hay que pulir)

---
+++ Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour {
+	
+	#region Public Attributes
+	public Texture fadeTexture;
+	public float fadeSpeed = 0.2f;
+	//0 -> transparent, 1 -> opaque
+	public float alpha = 1.0f;
+	public int fadeDir = -1;
+	#endregion
+
+	#region Private Attributes
+	private int drawDepth = -100;
+	private Color fadeColor;
+	#endregion
+	
+	#region MonoDevelop Methods	
+	// Update is called once per frame
+	void Update () 
+	{
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+		fadeColor = GUI.color;
+		fadeColor.a = alpha;
+	}
+
+	//
+	void OnGUI()
+	{
+		GUI.color = fadeColor;
+		GUI.depth = drawDepth;
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeTexture);
+	}
+	#endregion
+	
+	#region User Methods
+	//change the fade direction
+	public void Switch()
+	{
+		fadeDir *= -1;
+	}
+
+	//get the current transparency
+	public float GetAlpha()
+	{
+		return alpha;
+	}
+	#endregion
+}
---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Orb : MonoBehaviour {
+
+    //
+    private float direction = 1;
+    private float initialPosY;
+
+	// Use this for initialization
+	void Start () {
+        initialPosY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //
+        transform.position += new Vector3(0.0f, direction * 0.1f * Time.deltaTime, 0.0f);
+
+        //
+        if (Mathf.Abs(transform.position.y - initialPosY) > 0.1f)
+        {
+            direction *= -1;
+        }
+
+        //Rings
+        transform.Rotate(0.0f, 1.0f, 0.0f);
+    }
+
+    //
+    public void Activate()
+    {
+        SceneManager.LoadScene("CentralPlace");
+    }
+}
