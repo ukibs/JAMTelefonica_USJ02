ce18f89fd5f2a99df62eabcb426824e016e41208
ukibs <martincativiela@gmail.com>
2017-10-08 20:57:15 +0200

    Rematado puentes levadizos, montado un poco mas templo de luz

---
+++ Assets/Scripts/Puzzles/Drawbridge.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawbridge : MonoBehaviour {
+
+    public int switches;
+    public float rotatingSpeed = 1.0f;
+
+    private int activatedOnes = 0;
+    private bool opened = false;
+    private bool moving = false;
+    private float amountRotated = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving == true)
+        {
+            //Debug.Log("Moving ");
+            Debug.Log(transform.eulerAngles.x);
+            transform.Rotate(-rotatingSpeed * Time.deltaTime, 0.0f, 0.0f);
+            amountRotated += rotatingSpeed * Time.deltaTime;
+            if (amountRotated >= 90.0f)
+            {
+                moving = false;
+                opened = true;
+            }
+        }
+        //Testing
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            moving = true;
+            Debug.Log("Testing activation");
+        }
+    }
+
+    //
+    void CLick()
+    {
+        activatedOnes++;
+        if (activatedOnes == switches)
+        {
+            moving = true;
+        }
+    }
+}
