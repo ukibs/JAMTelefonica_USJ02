8755636c65b2ff557190571d4e6dd0ead5ebc201
Martín <martincativiela@gmail.com>
2017-10-08 11:13:51 +0200

    Templo de oscuridad funcional

---
+++ Assets/Scripts/Puzzles/Door.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public int switches;
+    public float distanceToMoveDown = 0.1f;
+    public float movingSpeed = 0.1f;
+
+    private int activatedOnes = 0;
+    private bool opened = false;
+    private bool moving = false;
+    private float originalY;
+
+	// Use this for initialization
+	void Start () {
+        originalY = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(moving == true)
+        {
+            Debug.Log("Moving");
+            transform.Translate(0.0f, -movingSpeed * Time.deltaTime, 0.0f);
+            if(originalY - transform.position.y > distanceToMoveDown)
+            {
+                moving = false;
+                opened = true;
+            }
+        }
+        //Testing
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            moving = true;
+            Debug.Log("Testing activation");
+        }
+
+	}
+
+    //
+    void CLick()
+    {
+        activatedOnes++;
+        if(activatedOnes == switches)
+        {
+            moving = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/Switch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    public GameObject receptor;
+
+    private bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //
+    public void Activate()
+    {
+        if (receptor && !activated)
+        {
+            receptor.transform.SendMessage("CLick");
+            activated = true;
+            Debug.Log("Activated");
+        }
+    }
+}
