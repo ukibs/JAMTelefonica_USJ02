aefd74a5f0f59ff51cf975e739e8a9af8bffff41
ukibs <martincativiela@gmail.com>
2017-10-09 12:39:45 +0200

    Comiteados archivos que faltaban

---
+++ Assets/Scripts/Puzzles/LightDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDoor : MonoBehaviour {
+
+    public GameObject lightToReceive;
+    public float distanceToMoveDown = 0.1f;
+    public float movingSpeed = 0.1f;
+    
+    private bool opened = false;
+    private bool moving = false;
+    private float originalY;
+
+    // Use this for initialization
+    void Start () {
+        originalY = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving == true)
+        {
+            Debug.Log("Moving");
+            transform.Translate(0.0f, -movingSpeed * Time.deltaTime, 0.0f);
+            if (originalY - transform.position.y > distanceToMoveDown)
+            {
+                moving = false;
+                opened = true;
+            }
+        }
+        //Testing
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            moving = true;
+            Debug.Log("Testing activation");
+        }
+        //
+        CheckLightToReceive();
+    }
+
+    //
+    void CheckLightToReceive()
+    {
+        if(lightToReceive.active == true && opened == false)
+        {
+            moving = true;
+        }
+    }
+}
